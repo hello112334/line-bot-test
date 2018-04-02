0aa602d6468df0fa6b714f7ffc7a52497a6c86a4
BooLuON <toyofumi1223@gmail.com>
2018-04-03 03:36:32 +0900

    Init

--- app.py
+++ app.py
@@ -38,8 +38,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    
+    message = TextSendMessage(text=event.message.text)  
     if message == "Hi":
         line_bot_api.reply_message(
         event.reply_token,
