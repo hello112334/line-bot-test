67d048ddec45457dd543e5047a1e94dd3cf063d2
BooLuON <toyofumi1223@gmail.com>
2018-04-03 07:00:39 +0900

    V2.4

--- app.py
+++ app.py
@@ -62,6 +62,7 @@ def handle_message(event):
             listA.append(item.string)  
         for name in listA:    
             line_bot_api.reply_message(event.reply_token, TextSendMessage(name))
+            continue
 
         #TextSendMessage('https://icook.tw/recipes/search?q=' + text + '&ingredients=')    
     else:
