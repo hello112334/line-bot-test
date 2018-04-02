c7283bc344f7be979239cb9f3d0ec0eef1eff189
BooLuON <toyofumi1223@gmail.com>
2018-04-03 03:42:36 +0900

    V1.0

--- app.py
+++ app.py
@@ -38,17 +38,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)  
-    if message == "Hi":
-        line_bot_api.reply_message(
-        event.reply_token,
-        "Hi, mate.")
-    else:
-        line_bot_api.reply_message(
+    message = TextSendMessage(text=event.message.text)
+    line_bot_api.reply_message(
         event.reply_token,
         message)
-        
-    
 
 import os
 if __name__ == "__main__":
