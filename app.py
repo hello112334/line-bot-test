e7ea4c54f7e9afbd31484592ba910b9f3dbd4437
BooLuON <toyofumi1223@gmail.com>
2018-04-03 04:03:08 +0900

    V0

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+    #message = TextSendMessage(text=event.message.text)
+    #line_bot_api.reply_message(event.reply_token, message)
     
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text='Hello World!'))  
         
